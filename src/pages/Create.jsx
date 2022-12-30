8c792aaaca056e11c9c37f86b279405778db5936
Rakaa <therajthombare@gmail.com>
2022-12-30 19:06:43 +0530

    add custom styles using useStyles

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,11 +1,29 @@
 import React from "react";
-import { Typography, Button, ButtonGroup, Container } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Typography, Button, Container } from "@mui/material";
 import ArrowForwardIos from "@mui/icons-material/ArrowForwardIos";
 
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: "violet",
+    "&:hover": {
+      background: "blue",
+    },
+  },
+
+  title: {
+    textDecoration: "underline",
+    marginBottom: 30,
+  },
+});
+
 const Create = () => {
+  const classes = useStyles();
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant="h6"
         component="h2"
         color="textSecondary"
@@ -15,6 +33,7 @@ const Create = () => {
       </Typography>
 
       <Button
+        className={classes.btn}
         variant="contained"
         color="primary"
         type="submit"
@@ -22,32 +41,6 @@ const Create = () => {
       >
         Submit
       </Button>
-
-      {/* <ButtonGroup color="secondary">
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup> */}
-
-      {/* <Typography
-        variant="h6"
-        component="h2"
-        color="textPrimary"
-        noWrap
-        gutterBottom
-      >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus ipsam
-        dolore quaerat, deserunt qui ad vero vitae iure cumque consectetur
-        corrupti possimus aliquid alias eligendi veniam blanditiis et eos quo
-        pariatur. Doloremque quod debitis soluta ut molestias blanditiis nobis
-        harum quisquam? Dicta corrupti cupiditate quod harum maiores blanditiis!
-        Aliquid dicta exercitationem itaque! Quam, non facere. Rem porro nostrum
-        sit sequi odio repellat molestias labore, excepturi totam consectetur
-        nam eius facere esse pariatur eaque reiciendis neque eos laboriosam
-        eveniet aut perferendis minima laudantium, accusantium ad! Est nesciunt
-        inventore, quasi tenetur odio ducimus quidem eveniet aperiam temporibus
-        dicta consequatur voluptatem quod corporis.
-      </Typography> */}
     </Container>
   );
 };
