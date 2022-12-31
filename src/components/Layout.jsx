c8335b5e288d8de4112cb0480d5f63b7a91f4d00
Rakaa <therajthombare@gmail.com>
2022-12-31 20:34:17 +0530

    add Drawer component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 import { makeStyles } from "@mui/styles";
+import { Typography } from "@mui/material";
+import Drawer from "@mui/material/Drawer";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
   page: {
     background: "#f9f9f9",
     width: "100%",
   },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: "flex",
+  },
 });
 
 const Layout = ({ children }) => {
   const classes = useStyles();
   return (
-    <div className={classes.page}>
-      <div>{children}</div>
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{ paper: classes.drawerPaper }}
+      >
+        <Typography variant="h5">Rakaa's Notes</Typography>
+      </Drawer>
+      <div className={classes.page}>
+        <div>{children}</div>
+      </div>
     </div>
   );
 };
