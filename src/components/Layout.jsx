f8913dec61b338355f6d89d88c6f08176cdb035e
Rakaa <therajthombare@gmail.com>
2023-01-01 00:27:45 +0530

    add routing to drawer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { makeStyles } from "@mui/styles";
-import { Typography } from "@mui/material";
-import Drawer from "@mui/material/Drawer";
+import {
+  Typography,
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+
+import { SubjectOutlined, AddCircleOutlineOutlined } from "@mui/icons-material";
 
 const drawerWidth = 240;
 
@@ -22,7 +31,22 @@ const useStyles = makeStyles({
 });
 
 const Layout = ({ children }) => {
+  const navigate = useNavigate();
   const classes = useStyles();
+
+  const menuItems = [
+    {
+      text: "My Notes",
+      icon: <SubjectOutlined color="primary" />,
+      path: "/",
+    },
+    {
+      text: "Add Note",
+      icon: <AddCircleOutlineOutlined color="primary" />,
+      path: "/create",
+    },
+  ];
+
   return (
     <div className={classes.root}>
       <Drawer
@@ -31,7 +55,22 @@ const Layout = ({ children }) => {
         anchor="left"
         classes={{ paper: classes.drawerPaper }}
       >
-        <Typography variant="h5">Rakaa's Notes</Typography>
+        <div>
+          <Typography variant="h5">Rakaa's Notes</Typography>
+        </div>
+
+        <List>
+          {menuItems.map((item) => (
+            <ListItemButton
+              button
+              key={item.text}
+              onClick={() => navigate(item.path)}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          ))}
+        </List>
       </Drawer>
       <div className={classes.page}>
         <div>{children}</div>
